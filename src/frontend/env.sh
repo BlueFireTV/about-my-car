ebe9345251840e921cb40c5d041a5925e896309f
studschumi1001 <michael.schurzmann@stud.th-rosenheim.de>
2025-03-24 20:55:00 +0100

    changes for build

---
+++ src/frontend/env.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+for i in $(env | grep WM_)
+do
+    key=$(echo $i | cut -d '=' -f 1)
+    value=$(echo $i | cut -d '=' -f 2-)
+    echo $key=$value
+    # sed All files
+    # find /usr/share/nginx/html -type f -exec sed -i "s|${key}|${value}|g" '{}' +
+
+    # sed JS and CSS only
+    find /etc/nginx/conf.d/ -type f -name 'default.conf' -exec sed -i "s|${key}|${value}|g" '{}' +
+done
+echo 'done'
