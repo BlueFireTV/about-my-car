896188888d5924d83cb9a6a4ac169e5cf8b30e83
studschumi1001 <michael.schurzmann@stud.th-rosenheim.de>
2025-03-25 08:05:12 +0100

    fix build

--- src/backend/src/users/users-index.ts
+++ src/backend/src/users/users-index.ts
@@ -1,15 +1,11 @@
 import express from 'express';
 import {
-    loginUser,
-    createUser,
-    updatePwUser
+    loginUser
     
 } from './users-controller';
 
 const usersRouter = express.Router();
 
 usersRouter.post('/login', loginUser );
-usersRouter.post('/register', createUser );
-usersRouter.post('/changepw', updatePwUser );
 
 export default usersRouter;
