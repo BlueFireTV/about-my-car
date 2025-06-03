2243aedc3308faf62f6e10f9ee2327a31101ccbf
studschumi1001 <michael.schurzmann@stud.th-rosenheim.de>
2025-06-03 18:16:04 +0200

    fix src

--- src/frontend/jest.config.js
+++ src/frontend/jest.config.js
@@ -3,7 +3,7 @@ export default {
   preset: 'ts-jest',
   // Change testEnvironment to jsdom for React/DOM tests
   testEnvironment: 'jsdom',
-  roots: ['<rootDir>/test', '<rootDir>/src'], 
+  roots: ['<rootDir>/tests', '<rootDir>/src'], 
   testMatch: ['**/tests/**/*.test.ts', '**/tests/**/*.test.tsx', '**/__tests__/**/*.test.ts', '**/__tests__/**/*.test.tsx'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   transform: {
